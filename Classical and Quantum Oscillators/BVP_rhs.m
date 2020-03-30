9d134aaeb0b18d83b6c9a0bafa2a905ca0c941a1
Lama <lamahamadeh1985@hotmail.com>
2020-03-30 11:21:10 +0100

    Add files via upload

---
+++ Classical and Quantum Oscillators/BVP_rhs.m
@@ -0,0 +1,28 @@
+
+
+
+% %%%Damped Harmonic Oscillator
+% %-----------------------------
+% function rhs = BVP_rhs(tspan,y0,dummy,d,w)
+%         %set the initial value for the position
+%         y1 = y0(1);
+%         %set the initial value for the velocity
+%         y2 = y0(2);
+%         %compute the equation
+%         rhs = [y2 ; -w^2*y1-d*y2];
+% 
+% end
+
+
+% %%%Van Der Pol Equation
+% %----------------------
+% function rhs = BVP_rhs(tspan,y0,dummy,mu)
+%         %set the initial value for the position
+%         y1 = y0(1);
+%         %set the initial value for the velocity
+%         y2 = y0(2);
+%         %compute the equation
+%         rhs = [y2 ; (mu-mu*y1^2)*y2-y1];
+% 
+% end
+
