35f9681006a59fe18d0d69845e8a9ac4ba5d9034
Lama <lamahamadeh1985@hotmail.com>
2020-04-16 11:32:23 +0100

    Update dTDistribution.m

--- Numerical Analysis of Heat Equation/FDM/dTDistribution.m
+++ Numerical Analysis of Heat Equation/FDM/dTDistribution.m
@@ -1,12 +1,9 @@
-
-
-
 %A function to caluclate the time-dependent temperature distribution (TDTD)
 
 %% Define the function
 %--------------------
-function dT=dTDistribution_3clouds(tspan,U,dummy,A,delta,D) 
+function dT=dTDistribution(tspan,T0,dummy,A,dx) 
 %the order after 'dummy'is important and should match with ode45 syntax
 
-dT = D*A*U; 
+dT = A*T0; 
 end
