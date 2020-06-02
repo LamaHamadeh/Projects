a1e76dcb1e36a93afd0a656bd7de73e37ef3f2a3
Lama <lamahamadeh1985@hotmail.com>
2020-06-02 21:05:30 +0100

    Update HeatConvolution.m

--- Numerical Analysis of Heat Equation/FFT vs FDM/HeatConvolution.m
+++ Numerical Analysis of Heat Equation/FFT vs FDM/HeatConvolution.m
@@ -1,31 +1,44 @@
 clear all;
 close all;
 
-%create a spatial grid
-N = 1001;
-L = 100;
-dx = L/(N);
-x = -L/2:dx:L/2;
-f = 0*x; %initial temperature distribution
-f((L/2 - L/10)/dx:(L/2 + L/10)/dx) = 1; %a square wave
+%Spatial variable on x direction
+L=20; %domain
+xmin=-L/2; %minimum boundary
+xmax=L/2; %maximum boundary 
+dx=0.05; %spatial step size
+N=(xmax-xmin)/dx; %number of spatial points
+x=linspace(xmin,xmax,N); %spatial vector
+
+%initialise temeprature distribution
+f = 0*x; 
+
+%initial condition: square function
+f((L/2 - L/10)/dx:(L/2 + L/10)/dx) = 1; 
+
+%plotting the initial condition
 plot(x,f)
 
-a = 3; %thermal diffusion constant
-y = x; %summy variable for the convolution integral
+%diffusion coeffieicnt
+a = 1; 
 
+%define the diffusion time step
 dt = 0.1;
+
+%define a dummy variable for the convolution integral
+y = x;
+
 for k = 1:100 %100 time steps
-    t = k*dt;
+    t = k*dt; %increase by one time step    
     for j = 1:length(x) %compute the convolution integral at each x point
         xi = x(j) - y;
         gxy = (1/(2*a*sqrt(pi*t)))*exp(-xi.^2/(4*a*a*t));
-        u(j) = dot(gxy,f)*dx; %temperature at the jth spatial locatiob
+        u(j) = dot(gxy,f)*dx; %temperature at the jth spatial location
     end
+    %plotting
     plot(x,u,'k')
     xlabel('Spatial variable, x')
     ylabel('Temperature, u(x,t)')
     title(['Time, t=',num2str(t)])    
     axis([-L/2 L/2 -.1 1.1])
-
     pause(0.1)
 end
