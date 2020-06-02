64fbf10517ab4654c8e8197cda686efd4f88fb16
Lama <lamahamadeh1985@hotmail.com>
2020-06-02 21:07:08 +0100

    Update HeatFFT.m

--- Numerical Analysis of Heat Equation/FFT vs FDM/HeatFFT.m
+++ Numerical Analysis of Heat Equation/FFT vs FDM/HeatFFT.m
@@ -1,33 +1,49 @@
 clear all;
 close all;
 
-%create a spatial grid
-N = 1001;
-L = 100;
-dx = L/(N);
-x = -L/2:dx:L/2;
-f = 0*x; %initial temperature distribution
-f((L/2 - L/10)/dx:(L/2 + L/10)/dx) = 1; %a square wave
+%Spatial variable on x direction
+L=20; %domain
+xmin=-L/2; %minimum boundary
+xmax=L/2; %maximum boundary 
+dx=0.05; %spatial step size
+N=(xmax-xmin)/dx; %number of spatial points
+x=linspace(xmin,xmax,N); %spatial vector
+
+%initialise temeprature distribution
+f = 0*x; 
+
+%initial condition: square function
+f((L/2 - L/10)/dx:(L/2 + L/10)/dx) = 1; 
+
+%plotting the initial condition
 plot(x,f)
 
-a = 1; %thermal diffusion constant
-y = x; %summy variable for the convolution integral
+%diffusion coeffieicnt
+a = 1; 
 
+%define the diffusion time step
 dt = 0.1;
-fhat = fft(f);
 
-for k = 1:100
-    t = k*dt; 
-    Nx = max(size(f));
-    kappa = (2*pi/L)*[-Nx/2:Nx/2-1];
-    kappa = fftshift(kappa);  % important because fft has weird ordering
-    uhat = fhat.*exp(-(a^2)*t*kappa.^2);
-    u = ifft(uhat);
-    plot(x,u,'k')
+%Define the wave vector
+k = (2*pi/L)*[0:(N/2-1) (-N/2):-1]; 
+%k(1) = 10^(-6);
+
+%solve the 1D diffusion equation as a function of time.
+Time = 100;
+
+for i = 0:Time     
+    t = i*dt; %increase by one time step   
+    %Fast Fourier transform of the function f
+    fhat = fft(f);
+    uhat = fftshift(fhat.*exp(-(a^2)*t*k.^2));    
+    %inverse of FT to the shifted function
+    u = ifft(uhat);    
+    %plotting
+    plot(x,abs(u),'k') %the absolute value of the function
     xlabel('Spatial variable, x')
     ylabel('Temperature, u(x,t)')
     title(['Time, t=',num2str(t)])
-    axis([-10 10 -.1 1.1])
-
+    axis([-15 15 -.1 1.1])
     pause(0.1)
 end
+
