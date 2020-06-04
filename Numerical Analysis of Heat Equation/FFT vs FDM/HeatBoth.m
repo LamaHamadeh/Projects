b2b4b39b4336b4d1ef8dd6be2c8bd666202bfaa7
Lama <lamahamadeh1985@hotmail.com>
2020-06-04 11:40:57 +0100

    Update HeatBoth.m

--- Numerical Analysis of Heat Equation/FFT vs FDM/HeatBoth.m
+++ Numerical Analysis of Heat Equation/FFT vs FDM/HeatBoth.m
@@ -1,39 +1,72 @@
 clear all;
 close all;
 
-%create a spatial grid
-N = 1001;
-L = 100;
-dx = L/(N);
-x = -L/2:dx:L/2;
-f = 0*x; %initial temperature distribution
-f((L/2 - L/10)/dx:(L/2 + L/10)/dx) = 1; %a square wave
-fhat = fft(f);
-a = 5;
-y = x;
+
+%Spatial variable on x direction
+L=100; %domain
+xmin=-L/2; %minimum boundary
+xmax=L/2; %maximum boundary 
+dx=0.05; %spatial step size
+N=(xmax-xmin)/dx; %number of spatial points
+x=linspace(xmin,xmax,N); %spatial vector
+%initialise temeprature distribution
+f = 0*x; 
+%initial condition: square function
+f((L/2 - L/10)/dx:(L/2 + L/10)/dx) = 1; 
+%diffusion coeffieicnt
+a = 10; 
+%define the diffusion time step
 dt = 0.1;
-for k = 1:100
-    t = k*dt;
-    for j = 1:length(x)
+%define a dummy variable for the convolution integral
+y = x;
+
+Time = 100;
+
+for i = 1:Time
+    
+    t = i*dt; %increase by one time step
+    
+    %Analytical: convolution integral
+    for j = 1:length(x) %compute the convolution integral at each x point
         xi = x(j) - y;
         gxy = (1/(2*a*sqrt(pi*t)))*exp(-xi.^2/(4*a*a*t));
-        u(j) = dot(gxy,f)*dx;
+        u(j) = dot(gxy,f)*dx; %temperature at the jth spatial location
     end
+    %plotting
     plot(x,u,'k')
     hold on
-    Nx = max(size(f));
-    kappa = (2*pi/L)*[-Nx/2:Nx/2-1];
-    kappa = fftshift(kappa);  % important because fft has weird ordering
-    uhat = fhat.*exp(-(a^2)*t*kappa.^2);
+    
+    %Numerical: Fourier transform
+    %method 1
+    %Fast Fourier transform of the function f
+    fhat = fft(f);
+    %Define the wave vector
+    k = (2*pi/L)*[-N/2:N/2-1]; 
+    k = fftshift(k);
+    uhat = fhat.*exp(-(a^2)*t*k.^2);
+    %inverse of FT to the shifted function
     u = ifft(uhat);
+    %plotting
     plot(x,u,'r--')
+
+%     %method 2
+%     %Fast Fourier transform of the function f
+%     fhat = fft(f);
+%      %Define the wave vector
+%     k = (2*pi/L)*[0:(N/2-1) (-N/2):-1]; %different than the previously defined k
+%     %k(1) = 10^(-6);
+%     uhat = fftshift(fhat.*exp(-(a^2)*t*k.^2));   
+%     %inverse of FT to the shifted function
+%     u = ifft(uhat);
+%     %plotting
+%     plot(x,abs(u),'r--')
+
     xlabel('Spatial variable, x')
     ylabel('Temperature, u(x,t)')
     title(['Time, t=',num2str(t)])
     hold off
     axis([-L/2 L/2 -.1 1.1])
-    legend('Analytical','Numerical') 
-    % to make them agree, we need to make the domain even longer
+    legend('Analytical','Numerical')
+    
     pause(0.1)
 end
-
