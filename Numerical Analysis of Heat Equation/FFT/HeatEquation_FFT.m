0572dd56d8c0f9f14bc59118834aef6df3c61ae2
Lama <lamahamadeh1985@hotmail.com>
2020-06-05 15:10:53 +0100

    Update HeatEquation_FFT.m

--- Numerical Analysis of Heat Equation/FFT/HeatEquation_FFT.m
+++ Numerical Analysis of Heat Equation/FFT/HeatEquation_FFT.m
@@ -3,10 +3,10 @@
 close all;
 clear all;
 
-           %%% Define Discretised spatial and time variables %%% 
-        
+                %%% Define 2D Discretised spatial %%%            
+
 %Spatial variable on x direction
-Lx=4; %LED's length
+Lx=2; %domain on x
 delta=0.05; %spatial step size
 xmin=-Lx; %minimum boundary
 xmax=Lx; %maximum boundary 
@@ -14,7 +14,7 @@ Nx=(xmax-xmin)/delta; %number of spatial points
 x=linspace(xmin,xmax,Nx); %spatial vector
 
 %Spatial variable on y direction
-Ly=4; %LED's length
+Ly=2; %domain on y
 delta=0.05; %spatial step size
 ymin=-Ly; %minimum boundary
 ymax=Ly; %maximum boundary 
@@ -23,12 +23,23 @@ y=linspace(ymin,ymax,Ny); %spatial vector
 
 %Total matrix size
 N = (Nx * Ny);
+%--------------------------
+                	     %%% 2D Initial state %%%
 
-%Time variable
-tspan = [0 0.01 0.05 0.1 0.2 0.5 0.6 0.8 1 1.5 2 2.5 3 3.5 4];
-%--------------
-
-                    %%% Wave vector disretisation %%%
+%initial consitions
+[X,Y] = meshgrid(x,y); %Nx X Ny grid
+%vorticity
+U = exp(-X.^2-Y.^2); %Gaussian
+% %plotting
+% figure(1)
+% pcolor(abs(U));
+% shading interp, colorbar, colormap jet
+% xlabel('x')
+% ylabel('y')
+% title('Initial Gaussian function')
+% set(gca,'FontSize',16)
+%--------------------------
+                    %%% 2D Wave vector disretisation %%%
                    
 %x direction
 kx = (2*pi/Lx)*[0:(Nx/2-1) (-Nx/2):-1]'; 
@@ -37,73 +48,52 @@ kx(1) = 10^(-6);
 ky = (2*pi/Ly)*[0:(Ny/2-1) (-Ny/2):-1]'; 
 ky(1) = 10^(-6);
 %to give kx and ky the sense of direction
-[KX,KY] = meshgrid(kx,ky);
+[KX,KY] = meshgrid(kx,ky); %N X N grid
+
 %convert to columns so they can pass to ode45
 KX = reshape(KX,N,1);
 KY = reshape(KY,N,1);
-%--------------
+%--------------------------
 
-                	     %%% Initial state %%%
-
-%initial consitions
-[X,Y] = meshgrid(x,y);
-%vorticity
-U = exp(-X.^2-Y.^2); %Gaussian
-%plotting
-%figure(1)
-%pcolor(abs(U));
-%shading interp
-%colorbar
-%colormap jet
+                    %%% Fast Fourier Transform %%%
+                    
 %FT+shift of the initial condition
 Ut = fftshift(fft2(U));   
 %stacked data to a coloumn to be passed later to ode45
 Ut = reshape(Ut,N,1);
-%--------------
-            
+%--------------------------
+
+                    %%% Time variable %%%
+
+%tspan = [0 0.01 0.05 0.1 0.2 0.5 0.6 0.8 1 1.5 2 2.5 3 3.5 4];
+dt = 0.1; %time step
+tmin = 0;
+tmax = 4;
+tspan = [tmin tmax];
+%--------------------------
+
                 %%%Iterate and integrate over time %%%
-                
-%diffusion coeffieicnt
-D = 0.00001;
-%solve
-[Time,Sol] = ode45('FFT_rhs',tspan,Ut,[], KX, KY,D);
-%--------------
-
-                        %%% Plotting %%%
-
-for i = 1:length(tspan)
-    subplot(3,5,i)
-    test = Sol(i,:);
-    test = ifft2(ifftshift(reshape(test,Nx,Nx))); 
-    pcolor(abs(test));
-    colormap jet
+   
+ for TimeIteration = 1:30
+    t= TimeIteration * dt;               
+    %solve
+    [Time,Sol] = ode45('FFT_rhs',tspan,Ut,[], KX, KY);
+    Sol = ifft2(ifftshift(reshape(Sol(TimeIteration,:),Nx,Nx))); 
+    surfl(abs(Sol));
+    colormap gray
     shading interp
+    colorbar
     axis square
-    drawnow;
-end
-%--------------
-
-                       %%% Make a video %%%
-figure;
-loops = size(Sol,1);
-F(loops) = struct('cdata',[],'colormap',[]);
-for i = 1:loops
-    test = Sol(i,:);
-    test = ifft2(ifftshift(reshape(test,Nx,Nx))); %iverse of the
-    %back-shifted solution
-    %pcolor(abs(test));
-    surfl(x,y,abs(test))
-    axis off
+    xlim ([0 80]);
+    ylim ([0 80]);
     zlim ([0, 1]); %This is very important so we can lock the zoom-in behaviour on the z axis
-    colormap jet
-    shading interp
+    xlabel('x')
+    ylabel('y')
+    zlabel('Solution')
+    title('Spectral Solution of the 2D Diffusion Equation')
+    set(gca,'FontSize',16)
+    txt = {['t = ' num2str(t)]};
+    text(60,40,1,txt,'FontSize',14)
     drawnow;
-    F(i) = getframe;
-end
-v = VideoWriter('Heat_FFT.avi');
-open(v);
-writeVideo(v, F);
-close(v);
-%--------------
-
-
+ end
+    %--------------------------
