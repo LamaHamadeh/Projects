ee232a64a673540d813518e20862676d93a0841a
Lama <lamahamadeh1985@hotmail.com>
2020-06-08 10:52:31 +0100

    Add files via upload

---
+++ Spectral Analysis/2D One-Way Wave Equation/FFT_rhs_2D.m
@@ -0,0 +1,13 @@
+
+function rhs = FFT_rhs_2D(tspan,Ut,dummy, KX, KY)
+
+%solve the right hand side
+rhs = -1i.*(KX+KY).*Ut;
+
+%To make the wave go to the other way, the right hand side can be put as:
+%rhs = 1i.*(KX+KY).*Ut;
+
+end
+
+
+
