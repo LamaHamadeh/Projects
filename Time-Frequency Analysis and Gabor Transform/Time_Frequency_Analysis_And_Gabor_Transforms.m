c326bf79cd53062efe45c86ab6f22b978cab074c
Lama <lamahamadeh1985@hotmail.com>
2020-06-08 15:22:52 +0100

    Update Time_Frequency_Analysis_And_Gabor_Transforms.m

--- Time-Frequency Analysis and Gabor Transform/Time_Frequency_Analysis_And_Gabor_Transforms.m
+++ Time-Frequency Analysis and Gabor Transform/Time_Frequency_Analysis_And_Gabor_Transforms.m
@@ -1,27 +1,26 @@
-
 close all;
 clear all;
 
-%space
-L = 10;
-%n = 2048;
-n = 60000;
-%time
+%Time domain
+L = 10; %is the range of the time domain
+%number of points in the time domain
+n = 2048; %number of points in the time domain
+%time variable
 t2 = linspace(0,L,n+1);
 t = t2(1:n);
 
 %arbitirary function/signal
-%S = (3*sin(2*t)+0.5*tanh(0.5*(t-3))+0.2*exp(-(t-4).^2)+1.5*sin(5*t)+4*cos(3*(t-6).^2))/10+(t/20).^3;
+S = (3*sin(2*t)+0.5*tanh(0.5*(t-3))+0.2*exp(-(t-4).^2)+1.5*sin(5*t)+4*cos(3*(t-6).^2))/10+(t/20).^3;
 
 %Quadratic Chirp signal
-%f0 = 50;
-%f1 = 250;
-%S = cos(2*pi*t.*(f0+(f1-f0)*t.^2/(3*L^2)));
+% f0 = 50;
+% f1 = 250;
+% S = cos(2*pi*t.*(f0+(f1-f0)*t.^2/(3*L^2)));
 
-%electric guitar solo (audio file)
-[y,fs] = audioread('dspafsx_mono.wav');
-sound(y,fs);
-S = y(1:n)';
+% %electric guitar solo (audio file)
+% [y,fs] = audioread('BoothBabyMantell.wav');
+% %sound(y,fs);
+% S = y(1:n)';
 
 %distinguish between hot and cold water
 %distinguish between male and female voices
@@ -55,22 +54,10 @@ xlabel('Frequency')
 g = exp((t-5).^2);
 Sg = g.*S; %Gabor Signal
 Sgt = fft(Sg); %Fourier Transofrm of the resulting signal
-%plotting
-figure(2)
-subplot(3,1,1)
-plot(t,S,'k',t,g,'r')
-title('Signal with Gabor function in time domain')
-subplot(3,1,2)
-plot(t,Sg,'k')
-title('Multiplication of the Signal with Gabor Transform')
-subplot(3,1,3)
-plot(ks,abs(fftshift(Sgt))/max(abs(Sgt)))
-axis([-50 50 0 1])
-title('Fourier Transofrm of the new signal')
 
 %sliding the window function along the signal
 Sgt_spec = [];
-tslide = 0:0.1:10; %increments in time
+tslide = 0:0.1:L; %increments in time
 for j=1:length(tslide)
 %Gabor Transform
 %window function
@@ -93,7 +80,7 @@ Sgt_spec = [Sgt_spec;abs(fftshift(Sgt))]; %matrix of spectrogram
 %with rows to the time snapshots and coloums of Gabor signal
 
 %plotting
-figure(3)
+figure(2)
 subplot(3,1,1)
 plot(t,S,'k',t,g,'r')
 title('Signal with Gabor function in time domain')
@@ -102,22 +89,19 @@ plot(t,Sg,'k')
 title('Multiplication of the Signal with Gabor Transform in time domain')
 subplot(3,1,3)
 plot(ks,abs(fftshift(Sgt))/max(abs(Sgt)))
-axis([-50 50 0 1])
+%axis([-50 50 0 1])
 title('Fourier Transofrm of the new signal in frequency domain')
 drawnow
 pause(0.1)
 end
 
 %plotting the spectrogram
-figure(4)
+figure(3)
 pcolor(tslide,ks,Sgt_spec.') 
 shading interp
-set(gca, 'Ylim', [-10000 10000])
-%set(gca, 'Ylim', [-50 50])
+% set(gca, 'Ylim', [-11 11])
+% %set(gca, 'Ylim', [-50 50])
 colormap(jet)
 xlabel('Time')
 ylabel('Frequency')
 title('Amplitude of a particular frequency at a particular time')
-
-
-
