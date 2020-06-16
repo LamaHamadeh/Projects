b21de9328636e403ef66a96acda1cb93d2b0c3a6
Lama <lamahamadeh1985@hotmail.com>
2020-06-16 15:13:24 +0100

    Update FFT_rhs_1D.m

--- Spectral Analysis/1D Burgers' Equation/FFT_rhs_1D.m
+++ Spectral Analysis/1D Burgers' Equation/FFT_rhs_1D.m
@@ -3,30 +3,30 @@ function rhs = FFT_rhs_1D(tspan,U,dummy,N,L)
 
 %%% Fast Fourier Transform to the initial condition %%%                   
 %FT+shift of the initial condition
-Ut = fft(U);   
+Ut = fftshift(fft(U));   
 
 %%% 1D Wave vector disretisation %%%                  
-%option1:
 k = (2*pi/L)*[0:(N/2-1) (-N/2):-1]; %already shifted frequency domain
 k(1) = 10^(-6);
+k = fftshift(k);
 k = reshape(k,N,1);
-%option2:
-% k = (2*pi/L)*[-N/2:N/2-1]; %needs shifting
-% k = fftshift(k);
-% k = reshape(k,N,1);
 
 %first derivative (advection)
 duhat = 1i .*k .*Ut;
 %inverse of FT
-du = ifft(duhat);
+du = ifft(ifftshift(duhat));
 
 %second derivative (diffusion)
 dduhat = -(k.^2) .*Ut;
 %inverse of FT
-ddu = ifft(dduhat);
+ddu = ifft(ifftshift(dduhat));
 
 %define diffusion coeffieicnt
-diffusion  = 0.01;
+%the larger the diffusion coeffiecient, the smaller the shock wave produced.
+%i.e., less dynamics as the diffusion coefficient supresses the production
+%of the shock wave.
+diffusion  = 0.005;
+
 %solve the right hand side
 rhs = -U .*du + diffusion .*ddu;
 
