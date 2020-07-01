1e510efed9b496a101458db1c64c38079cff82b8
Lama <lamahamadeh1985@hotmail.com>
2020-07-01 09:44:31 +0100

    Update FFT_rhs_1D.m

--- Spectral Analysis/1D Korteweg–de Vries Equation/FFT_rhs_1D.m
+++ Spectral Analysis/1D Korteweg–de Vries Equation/FFT_rhs_1D.m
@@ -1,15 +1,33 @@
 
-function rhs = FFT_rhs_1D(tspan,U,dummy,du,dddu)
+function rhs = FFT_rhs_1D(tspan,U,dummy,N,L)
 
+
+%Fast Fourier Transform to the initial condition                 
+Ut = fftshift(fft(U));   
+% Ut = reshape(Ut,N,1);
+
+%1D Wave vector disretisation              
+k = (2*pi/L)*[0:(N/2-1) (-N/2):-1];
+k(1) = 10^(-6);
+% k = (2*pi/L)*(-N/2:N/2-1)';
+k = fftshift(k);
+k = reshape(k,N,1);
+
+%first derivative (advection)
+duhat = 1i *k .*Ut;
+du = real(ifft(fftshift(duhat))); %inverse of FT
+
+%third derivative (diffusion)
+ddduhat = -1i * (k.^3) .*Ut;
+dddu = real(ifft(fftshift(ddduhat))); %inverse of FT
 
 %define diffusion coeffieicnt
-diffusion  = 0.0005;
-% diffusion = (0.022)^2;
+diffusion  = 1;
+advection = 6;
 
 %solve the right hand side
-rhs = - U .* du + diffusion .* dddu;
+rhs = - advection .* U .* du + diffusion .* dddu;
 
 end
-
 
 
