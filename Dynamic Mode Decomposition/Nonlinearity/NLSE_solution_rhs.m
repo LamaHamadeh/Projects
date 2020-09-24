59eeceeb277cdbf0fd5ab858df07259ad48c7620
Lama <lamahamadeh1985@hotmail.com>
2020-09-24 10:25:09 +0100

    Update NLSE_solution_rhs.m

--- Dynamic Mode Decomposition/Nonlinearity/NLSE_solution_rhs.m
+++ Dynamic Mode Decomposition/Nonlinearity/NLSE_solution_rhs.m
@@ -1,7 +1,7 @@
 
-
+%i*ut+(1/2)*uxx+u.*|u|^2=0
 function rhs = NLSE_solution_rhs(t,ut,dummy,k)
-
+%inverse of Fourier Transform of u
 u = ifft(ut);
-
+%solve the right hand side
 rhs = -(1i/2)*(k.^2).*ut+1i*fft((abs(u).^2).*u);
